7ee272324a1603610e22520fd09ea668e61e35f6
vanianavarreteh <vania.navarrete2022@umce.cl>
2025-07-17 21:32:14 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-
+const ramos = [
+  {
+    "codigo": "QQUI101",
+    "nombre": "Química General I",
+    "semestre": 1,
+    "prerrequisitos": [],
+    "abre": [
+      "QQUI202"
+    ],
+    "creditos": 9
+  },
+  {
+    "codigo": "FPED126",
+    "nombre": "Pedagogía e identidad profesional docente",
+    "semestre": 1,
+    "prerrequisitos": [],
+    "abre": [
+      "QQUI204"
+    ],
+    "creditos": 4
+  },
+  {
+    "codigo": "QQUI103",
+    "nombre": "Habilidades Comunicacionales",
+    "semestre": 1,
+    "prerrequisitos": [],
+    "abre": [],
+    "creditos": 4
+  },
+  {
+    "codigo": "QQUI104",
+    "nombre": "Introducción al Aprendizaje de las Ciencias",
+    "semestre": 1,
+    "prerrequisitos": [],
+    "abre": [],
+    "creditos": 4
+  },
+  {
+    "codigo": "QQUI202",
+    "nombre": "Química General II",
+    "semestre": 2,
+    "prerrequisitos": [
+      "QQUI101"
+    ],
+    "abre": [],
+    "creditos": 8
+  },
+  {
+    "codigo": "QQUI204",
+    "nombre": "Práctica I: Interacción con el Contexto Educativo",
+    "semestre": 2,
+    "prerrequisitos": [
+      "FPED126"
+    ],
+    "abre": [],
+    "creditos": 2
+  }
+];
